737af2f56c4120818b4c2ddea8d802c035256ab8
modestas <mbujanauskas@gmail.com>
2018-09-24 15:23:51 +0300

    Add kontaktai, mokslas, darbuotojai, istorija, lab., stojantiems, studijos html files.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,17 @@
 function addFixedTop() {
     try {
         const header = document.querySelector('header');
+        console.log(header.offsetHeight)
+        const nextElement = document.querySelector('header + section')
+        console.log(nextElement)
+
         window.addEventListener('scroll', () => {
             header.classList.add("fixed-top");
+            nextElement.style.marginTop = `${header.offsetHeight}px`
             if(window.pageYOffset === 0) {
-                header.classList.remove("fixed-top");
+                header.classList.remove("fixed-top")
+                nextElement.style.marginTop = `0`
+
             }
         }); 
     } catch (error) {
